ac0fb4d0f38c0ce7256895eef71cb29d08ea51cd
Jacqueline D <jacdagdigian@gmail.com>
2020-05-19 09:13:32 -0700

    Added error handling.

--- TwoSum/TwoSum/main.cpp
+++ TwoSum/TwoSum/main.cpp
@@ -7,7 +7,6 @@
 //
 /*
      Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
      You may assume that each input would have exactly one solution, and you may not use the same element twice.
  */
 
@@ -22,12 +21,18 @@ vector<int> twoSum(vector<int>&, int);
 int main(int argc, const char * argv[])
 {
     vector<int> testArray{4, 5, 8, 12, 2, 3};
-    vector<int> targets{9, 11, 15, 1, 4};
+    vector<int> targets{9, 11, 15, 1, 4, -1};
 
     for(int i = 0; i < targets.size(); i++)
     {
-        cout << "Test Case " << (i+1) << ":\nTarget: " << targets[i] << "\nResult: "
-            << twoSum(testArray, targets[i])[0] << ", " << twoSum(testArray, targets[i])[1] << "\n\n";
+        try{
+            cout << "Test Case " << (i+1) << ":\nTarget: " << targets[i] << "\nResult: "
+                << twoSum(testArray, targets[i])[0] << ", " << twoSum(testArray, targets[i])[1] << "\n\n";
+        }
+        catch(char const* e)
+        {
+           cout << e << endl << endl;
+       }
     }
     
     return 0;
@@ -53,5 +58,5 @@ int main(int argc, const char * argv[])
         umap.emplace(nums.at(i), i);
     }
 
-    return {index1, index2};
+    throw "No result found.";
  }
